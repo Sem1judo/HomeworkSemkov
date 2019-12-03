83e08c29a7b99713bc72187671779ca49b457dbd
Stanislav <judo1stss1@gmail.com>
2019-12-03 16:45:33 +0200

    Task 1

---
+++ src/Task_1_3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Task_1_3 {
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    public static int scanNumber() {
+        System.out.print("Please enter your height yours triangle: ");
+        return scanner.nextInt();
+    }
+
+    public static void draw() {
+        int noOfRows = scanNumber();
+
+        int rowCount = 1;
+        System.out.println("Here Is Your Pyramid");
+
+        for (int i = noOfRows; i > 0; i--) {
+            for (int j = 1; j <= i * 2; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= rowCount; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = rowCount - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+
+            rowCount++;
+        }
+
+}
+
+
+    public static void main(String[] args) {
+        draw();
+    }
+}
