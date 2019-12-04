0d03efdf53ecc1a58cbd19ee60cdd016eb48993c
Stanislav <judo1stss1@gmail.com>
2019-12-04 19:40:20 +0200

    redevelop 1_3, added verify

--- src/Task_1_3.java
+++ src/Task_1_3.java
@@ -5,34 +5,44 @@ public class Task_1_3 {
 
 
     public static int scanNumber() {
-        System.out.print("Please enter your height yours triangle: ");
+        System.out.print("Please enter your triangle height in range 1-9: ");
         return scanner.nextInt();
     }
 
     public static void draw() {
-        int noOfRows = scanNumber();
 
         int rowCount = 1;
 
-        for (int i = noOfRows; i > 0; i--) {
-            for (int j = 1; j <= i * 2; j++) {
-                System.out.print(" ");
+        boolean flag = false;
+
+        do {
+            int noOfRows = scanNumber();
+            if ((noOfRows <= 9) && (noOfRows >= 1)){
+                for (int i = noOfRows; i > 0; i--) {
+                    for (int j = 1; j <= i * 2; j++) {
+                        System.out.print(" ");
+                    }
+
+                    for (int j = 1; j <= rowCount; j++) {
+                        System.out.print(j + " ");
+                    }
+
+                    for (int j = rowCount - 1; j >= 1; j--) {
+                        System.out.print(j + " ");
+                    }
+
+                    System.out.println();
+
+                    rowCount++;
+                    flag = true;
+                }
+            }else {
+                System.out.println("Wrong, height have to be between 1-9\nTry enter height again ");
             }
+        } while (!flag);
 
-            for (int j = 1; j <= rowCount; j++) {
-                System.out.print(j + " ");
-            }
 
-            for (int j = rowCount - 1; j >= 1; j--) {
-                System.out.print(j + " ");
-            }
-
-            System.out.println();
-
-            rowCount++;
-        }
-
-}
+    }
 
 
     public static void main(String[] args) {
